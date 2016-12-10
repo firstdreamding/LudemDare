acbe4a520811fd78e502d45e999a400b1642b0c0
gemeLn <mtang317@gmail.com>
2016-12-09 23:40:24 -0600

    11:40

--- src/entities/Projectile.java
+++ src/entities/Projectile.java
@@ -8,14 +8,12 @@ public class Projectile extends Entity {
 	public long start;
 	public Hitbox hit;
 
-	public Projectile(int x, int y, int w, int h, int xvel, int yvel, int damage, int dur, String s, int dir) {
+	public Projectile(int x, int y, int w, int h, int xvel, int yvel, int damage, int dur, String s) {
 		super(x, y, w, h, 0, new Texture("/res/sprites/" + s + ".png", w, h));
 		marginX = 0;
 		marginY = 0;
 		radius = 0;
-		System.out.println("/res/sprites/" + s + ".png");
 		this.x = x;
-		this.dir = dir;
 		this.y = y;
 		this.damage = damage;
 		this.dur = dur;
--- src/main/Level.java
+++ src/main/Level.java
@@ -4,8 +4,10 @@ import java.awt.Color;
 import java.awt.Font;
 
 import controller.HitboxController;
+import controller.ProjectileController;
 import entities.Hitbox;
 import entities.Player;
+import entities.Projectile;
 import graphics.Screen;
 import graphics.Texture;
 
@@ -14,15 +16,18 @@ public class Level {
 	Texture bg;
 	Texture coin;
 	HitboxController hbc;
+	ProjectileController pc;
 	Font defFont;
 	int wave = 0;
 	boolean inWave = false;
-	
+
 	public Level() {
-		bg = new Texture("/res/bg.png", 960, 540);
-		coin = new Texture("/res/coin.png", 40, 40);
-		player = new Player(0, 50, 0, 50, 50, 1, new Texture("/res/1default.png",50, 50));
+		bg = new Texture("/res/sprites/bg.png", 960, 540);
+		coin = new Texture("/res/sprites/coin.png", 40, 40);
+		player = new Player(0, 50, 0, 50, 50, 1, new Texture("/res/sprites/1default.png", 50, 50));
 		hbc = new HitboxController();
+		pc = new ProjectileController();
+		Projectile p = new Projectile(10, 10, 20, 20, 20, 0, 10, 2000, "coin");
 		defFont = new Font("Garamond", 1, 50);
 		hbc.add(new Hitbox(10, 0, 0, 50, 50, 0, 0, 1000, player, false), 0);
 	}
@@ -31,15 +36,15 @@ public class Level {
 		screen.drawTexture(0, 100, bg);
 		screen.drawTexture(player.getX(), player.getY(), player.getTexture());
 		screen.drawTexture(10, 20, coin);
-		screen.drawString(String.valueOf(player.gold), 60, 55,defFont,Color.RED);
+		screen.drawString(String.valueOf(player.gold), 60, 55, defFont, Color.RED);
 		hbc.render(screen);
 	}
-	
+
 	public void update(Screen screen) {
 		player.update();
 		hbc.update();
 		render(screen);
-		
+
 	}
 
 	public Player getPlayer() {
