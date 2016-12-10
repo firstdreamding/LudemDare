b02430b04201baa66827ef33776dca930a8d28be
gemeLn <mtang317@gmail.com>
2016-12-09 22:39:53 -0600

    10:50

--- src/main/Level.java
+++ src/main/Level.java
@@ -1,5 +1,8 @@
 package main;
 
+import java.awt.Color;
+import java.awt.Font;
+
 import controller.HitboxController;
 import entities.Player;
 import graphics.Screen;
@@ -10,6 +13,7 @@ public class Level {
 	Texture bg;
 	Texture coin;
 	HitboxController hbc;
+	Font defFont;
 	int wave = 0;
 	boolean inWave = false;
 
@@ -18,17 +22,19 @@ public class Level {
 		coin = new Texture("/res/coin.png", 40, 40);
 		player = new Player(0, 100, 0, 100, 100, 1, new Texture("/res/1default.png", 100, 100));
 		hbc = new HitboxController();
+		defFont = new Font("Garamond", 1, 50);
 	}
 
 	public void render(Screen screen) {
 		screen.drawTexture(player.getX(), player.getY(), player.getTexture());
 		screen.drawTexture(10, 20, coin);
-		screen.drawString(String.valueOf(player.gold), 60, 20);
+		screen.drawString(String.valueOf(player.gold), 60, 55,defFont,Color.RED);
 	}
-
+	
 	public void update(Screen screen) {
 		render(screen);
 		player.update();
+		player.gold++;
 	}
 
 	public Player getPlayer() {
