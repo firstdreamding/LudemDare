776f21bdf5f9796b383cd4807134c3c69a2b89a6
firstdreamding <huaxu3721@yahoo.com>
2016-12-09 22:10:05 -0600

    10 09

---
+++ src/menu/GoldMenu.java
@@ -0,0 +1,16 @@
+package menu;
+
+import graphics.Screen;
+
+public class GoldMenu extends Menu {
+	public void update() {
+	}
+
+	public void render(Screen screen) {
+	}
+
+	public void reset() {
+
+	}
+
+}
---
+++ src/menu/Menu.java
@@ -0,0 +1,17 @@
+package menu;
+
+import graphics.Screen;
+
+public abstract class Menu {
+
+	public void update() {
+	}
+
+	public void render(Screen screen) {
+	}
+
+	public void reset() {
+
+	}
+
+}
