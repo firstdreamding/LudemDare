17bf06243807fb8eed19cb22ebc6d0b56ca3d76b
gemeLn <mtang317@gmail.com>
2016-12-09 22:20:32 -0600

    10:20

---
+++ src/entities/Weapon.java
@@ -0,0 +1,14 @@
+package entities;
+
+public class Weapon {
+	int damage;
+	int cost;
+	int id;
+
+	public Weapon(int damage, int cost, int id) {
+		this.damage = damage;
+		this.cost = cost;
+		this.id = id;
+
+	}
+}
--- src/main/Level.java
+++ src/main/Level.java
@@ -8,18 +8,22 @@ import graphics.Texture;
 public class Level {
 	Player player;
 	Texture bg;
+	Texture coin;
 	HitboxController hbc;
 	int wave = 0;
 	boolean inWave = false;
 
 	public Level() {
 		bg = new Texture("/res/bg.png", 960, 540);
+		coin = new Texture("/res/coin.png", 40, 40);
 		player = new Player(0, 100, 0, 100, 100, 1, new Texture("/res/1default.png", 100, 100));
 		hbc = new HitboxController();
 	}
 
 	public void render(Screen screen) {
 		screen.drawTexture(player.getX(), player.getY(), player.getTexture());
+		screen.drawTexture(10, 20, coin);
+		screen.drawString(String.valueOf(player.gold), 60, 20);
 	}
 
 	public void update(Screen screen) {
@@ -29,7 +33,6 @@ public class Level {
 
 	public Player getPlayer() {
 		return player;
-
-	};
+	}
 
 }
